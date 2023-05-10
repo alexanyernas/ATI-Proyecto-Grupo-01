3daefa3b8afc201989508729914474a3a6745175
Alexanyer Naranjo <alexanyernaranjo@gmail.com>
2023-05-10 12:58:08 -0400

    Add New Routes and Bootstrap

--- src/app.py
+++ src/app.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python
-import flask
+import flask 
 
 app = flask.Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!\n'
+def home_controller():
+    return flask.render_template('home/homePage.html')
+
+@app.route('/auth/login')
+def login_controller():
+    return flask.render_template('auth/loginPage.html')
+
+@app.route('/auth/register')
+def register_controller():
+    return flask.render_template('auth/registerPage.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')     # open for everyone
